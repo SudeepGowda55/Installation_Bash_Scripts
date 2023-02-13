d80fe7aebca0bf6bda5427ccf00dc0c4be7c770b
SudeepGowda55 <ssudeepgowda55@gmail.com>
2023-02-13 08:50:06 +0530

    docker installation script added

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-read -p "Please Enter the subnet you want to scan Ex:- 192.168.1 : " SUBNET
+# read -p "Please Enter the subnet you want to scan Ex:- 192.168.1 : " SUBNET
 
-for IP in $( seq 1 254 );
-do 
-    echo $( ping -c 1 $SUBNET.$IP )
-done
+# for IP in $( seq 1 254 );
+# do 
+#     echo $( ping -c 1 $SUBNET.$IP )
+# done
     
+echo "THE $0"
+echo "these $1"
