aa56675948c8271d9e34edee8ad47512dce597cc
SudeepGowda55 <ssudeepgowda55@gmail.com>
2023-04-10 17:48:26 +0530

    mainnet updated

--- bitcoin_full_node.sh
+++ bitcoin_full_node.sh
@@ -2,8 +2,6 @@
 
 sudo apt-get update
 
-sudo apt install git build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev
-
 wget https://bitcoin.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
 
 tar -xzf bitcoin-22.0-x86_64-linux-gnu.tar.gz
@@ -14,4 +12,10 @@ sudo apt-get update
 
 rm -r bitcoin-22.0-x86_64-linux-gnu.tar.gz
 
-bitcoind -testnet -daemon
+#  to run and interact with testnet 
+
+# bitcoind -testnet -daemon
+
+# to run and interact with mainnet
+
+bitcoind -daemon
